e0fdac5c96424e6ba5253d4ce12be3dbbe1da819
Tengo Gogisvanidze <tengo6531@gmail.com>
2024-05-08 20:08:03 +0400

    cookies.....

--- src/index.mjs
+++ src/index.mjs
@@ -1,32 +1,19 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3700;
 
 app.use(express.json());
-
+app.use(cookieParser('helloworld'));
 app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
 });
 
-app.get(
-  "/",
-  (request, response, next) => {
-    console.log("Base URL 1");
-    next();
-  },
-  (request, response, next) => {
-    console.log("Base URL 2");
-    next();
-  },
-  (request, response, next) => {
-    console.log("Base URL 3");
-    next();
-  },
-  (request, response) => {
-    response.status(201).send({ msg: "Hello World" });
-  }
-);
+app.get("/", (request, response) => {
+  response.cookie("hello", "world", { maxAge: 30000, signed: true});
+  response.status(201).send({ msg: "Hello World" });
+});
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-  response.send([{ id: 123, name: "Chicken Breast", price: 12.99 }]);
+  console.log(request.headers.cookie);
+  console.log(request.cookies);
+  console.log(request.signedCookies);
+  if (request.cookies.hello && request.cookies.hello == "world") {
+    return response.send([{ id: 123, name: "Chicken Breast", price: 12.99 }]);
+  }
+  return response
+    .status(403)
+    .send({ msg: "Sorry. You need the correct cookie" });
 });
 
 export default router;
