7c31a1eab1e5161126bceaec328d389dc5924279
Tengo Gogisvanidze <tengo6531@gmail.com>
2024-05-10 13:12:08 +0400

    ...

--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,10 +3,10 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-  console.log(request.headers.cookie);
-  console.log(request.cookies);
-  console.log(request.signedCookies);
-  if (request.cookies.hello && request.cookies.hello == "world") {
+  console.log(111,request.headers.cookie);
+  console.log(222,request.cookies);
+  console.log(3333,request.signedCookies);
+  if (request.signedCookies.hello && request.signedCookies.hello == "world") {
     return response.send([{ id: 123, name: "Chicken Breast", price: 12.99 }]);
   }
   return response
